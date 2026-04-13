6aff4dd39155a9047c5b4aac7e6270b0dc437cb3
RenanCruz7 <renancruz199@gmail.com>
2026-04-13 16:15:26 -0300

    Initial Commit

---
+++ auth.service/src/test/java/auth/auth/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package auth.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
